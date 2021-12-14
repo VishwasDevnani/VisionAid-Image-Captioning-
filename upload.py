3321ca49a2be771a1a349e72564c0e179d8c1a31
Vishwas-Devnani <devishu13@gmail.com>
2021-12-14 23:17:36 +0530

    requirements added

--- upload.py
+++ upload.py
@@ -8,43 +8,55 @@ import os
 import time
 
 
-st.title("Upload + Classification Example")
+option = st.radio('', ['Choose a Sample XRay', 'Upload your own XRay'])
+
+st.title("Image Captioning")
+
+if option == 'Choose a Sample XRay':
+    tokenizer = load(open('tokenizer.pkl', 'rb'))
+    max_length = 34
+
+    filename = 'model_19.h5'
+    model = load_model(filename)
+
+    #####################################
+    test_imgs = os.listdir("test_images/")
+    test_img = st.selectbox(
+        'Please Select a Test Image:',
+        test_imgs
+    )
 
 
-tokenizer = load(open('tokenizer.pkl', 'rb'))
-max_length = 34
-
-filename = 'model_19.h5'
-model = load_model(filename)
-
-#####################################
-test_imgs = os.listdir("test_images/")
-test_img = st.selectbox(
-    'Please Select a Test Image:',
-    test_imgs
-)
-
-# Display and then predict on that image
-fl_path = "test_images/"+test_img
-# img = open_image(fl_path)
-
-display_img = mpimg.imread(fl_path)
-st.image(display_img, caption="Chosen Image", use_column_width=True)
-
-st.write("")
-with st.spinner("Identifying the Image"):
-    time.sleep(5)
-# label, prob = infer(img)
-# st.success(f"Image : {label}, Confidence: {prob:.2f}%")
+    fl_path = "test_images/"+test_img
 
 
-# uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+    display_img = mpimg.imread(fl_path)
+    st.image(display_img, caption="Chosen Image", use_column_width=True)
+
+    st.write("")
+    with st.spinner("Identifying whats going on in the image"):
+        time.sleep(5)
 
 
-# path='C:\\Users\\acer\\Desktop\\Image Captioning\\Flickr8k_Dataset\\Flicker8k_Dataset\\'
+    up = extract_features(fl_path)
 
-up = extract_features(fl_path)
+    label = generate_desc(model, tokenizer, up, max_length))
+    st.write(label[8:-6])
 
-label = generate_desc(model, tokenizer, up, max_length)
-# st.write('%s (%.2f%%)' % (label[1], label[2]*100))
-st.write(label[8:-6])
+else:
+    uploaded_file = st.file_uploader("Choose an Image", type=['jpg', 'png', 'jpeg'])
+    if uploaded_file is not None:
+        img = Image.open(uploaded_file)
+        st.image(img, caption="Uploaded Image", use_column_width=True)
+        
+        # img = image.pil2tensor(img, np.float32).div_(255)
+        # img = image.Image(img)
+        st.write("")
+        with st.spinner("Identifying whats going on in the image"):
+            time.sleep(5)
+        up = extract_features(img)
+
+        label = generate_desc(model, tokenizer, up, max_length))
+        st.write(label[8:-6])
+        
+        # st.success(f"Image Disease: {label}, Confidence: {prob:.2f}%")
