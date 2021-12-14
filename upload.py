77182529d73fc6b611d65ed0e6ec6767bb71bab2
Vishwas-Devnani <devishu13@gmail.com>
2021-12-14 22:55:49 +0530

    requirements added

--- upload.py
+++ upload.py
@@ -3,6 +3,7 @@ from PIL import Image
 from classify import generate_desc,extract_features
 from pickle import load
 from keras.models import Model, load_model
+import matplotlib.image as mpimg
 
 
 st.title("Upload + Classification Example")
@@ -14,16 +15,34 @@ max_length = 34
 filename = 'model_19.h5'
 model = load_model(filename)
 
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+#####################################
+test_imgs = os.listdir("test_images/")
+test_img = st.selectbox(
+    'Please Select a Test Image:',
+    test_imgs
+)
 
-if uploaded_file is not None:
-    path='C:\\Users\\acer\\Desktop\\Image Captioning\\Flickr8k_Dataset\\Flicker8k_Dataset\\'
-    st.write(uploaded_file.name)
-    up = extract_features(path+uploaded_file.name)
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
-    st.write("")
-    st.write("Classifying...")
-    label = generate_desc(model, tokenizer, up, max_length)
-    # st.write('%s (%.2f%%)' % (label[1], label[2]*100))
-    st.write(label[8:-6])
+# Display and then predict on that image
+fl_path = "test_images/"+test_img
+img = open_image(fl_path)
+
+display_img = mpimg.imread(fl_path)
+st.image(display_img, caption="Chosen Image", use_column_width=True)
+
+st.write("")
+with st.spinner("Identifying the Image"):
+    time.sleep(5)
+# label, prob = infer(img)
+# st.success(f"Image : {label}, Confidence: {prob:.2f}%")
+
+
+# uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+
+# path='C:\\Users\\acer\\Desktop\\Image Captioning\\Flickr8k_Dataset\\Flicker8k_Dataset\\'
+
+up = extract_features(fl_path)
+
+label = generate_desc(model, tokenizer, up, max_length)
+# st.write('%s (%.2f%%)' % (label[1], label[2]*100))
+st.write(label[8:-6])
